28bf7484a6f908bf2a1bd6517b3d7ae011f63046
laigubiao <2167027588@qq.com>
2025-04-20 16:16:16 +0800

    Update chessboard.h

--- chessboard.h
+++ chessboard.h
@@ -2,13 +2,18 @@
 #define CHESSBOARD_H
 #include "chess.h"
 #include <vector>
+#include <fstream>
 using namespace std;
 class chessboard
 {
 private:
 	int width;
 	int height;
+	int incoordinate[2][2];
+	int bocoordinate[2];
+	int cacoordinate[2][2];
 	vector<vector<chess*>>winposi;
+	vector<vector<chess*>>initialposit;
 	vector<vector<chess*>>board;
 public:
 	chessboard() 
@@ -23,12 +28,28 @@ public:
 		{
 			board.push_back({ nullptr,nullptr,nullptr,nullptr,nullptr });
 		}
+		for (int i = 0; i < 5; i++)
+		{
+			initialposit.push_back({ nullptr,nullptr,nullptr,nullptr,nullptr });
+		}
+		for (int i = 0; i < 2; i++)
+		{
+			bocoordinate[i] = 0;
+			for (int j = 0; j < 2; j++)
+			{
+				incoordinate[i][j] = 0;
+				cacoordinate[i][j] = 0;
+			}
+		}
 	};
 	chessboard(vector<vector<chess*>>&initialposi, vector<vector<chess*>>&inwinposi);
 	void loadwithfile(ifstream& ifs);
 	void wincondition();
 	void printboard();
-	void useroperate();
 	bool checksuccess();
+	void reset();
+	void getcoor();
+	void useroperate();
+	~chessboard() {};
 };
 #endif
