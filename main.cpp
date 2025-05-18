a3e08bb292c8bb4ac853d8b2de403d9e07625d5f
laigubiao <2167027588@qq.com>
2025-05-18 18:09:16 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,10 @@
 #include "chessboard.h"
 static string recordpassword;
 static string recordid;
-ofstream ofs("user.txt");
-ifstream ifs("user.txt");
+static const string filename="user.txt";
 int main()
 {
 	user comup;
-	comup.loginaccount(recordpassword,recordid,ifs,ofs);
+	comup.loginaccount(recordpassword,recordid,filename);
 	comup.interface();
 }
